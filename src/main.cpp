208171283298b5d203e10672a1f08392c0520b23
Nat <nat@cmmc.io>
2018-08-20 19:01:29 +0700

    add necessary.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 
 #include <CMMC_Legend.h>
-#include <CMMC_Sensor.h>
 #include "modules/LatteModule.h"
 #include "modules/WiFiModule.h"
 
@@ -8,7 +7,7 @@ CMMC_Legend os;
 
 void setup()
 {
-  // os.addModule(new WiFiModule());
+  os.addModule(new WiFiModule());
   os.addModule(new LatteModule());
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
